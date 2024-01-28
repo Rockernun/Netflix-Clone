dff820fc23410306b464270214712b46f91ea4c9
Eokji coding <byunguk98@naver.com>
2024-01-28 19:31:58 +0900

    useNavigate hook
    react-router의 navigate hook을 사용하여 검색한 영화의 URL로 이동

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
-import { Link, useMatch } from "react-router-dom";
+import { Link, useMatch, useNavigate } from "react-router-dom";
 import { motion, useAnimation, useMotionValueEvent, useScroll } from "framer-motion";
 import { useState, useEffect } from "react";
+import { useForm } from 'react-hook-form';
+
 
 const Nav = styled(motion.nav)`
   display: flex;
@@ -49,7 +51,7 @@ const Item = styled.li`
   }
 `;
 
-const Search = styled.span`
+const Search = styled.form`
   color: white;
   display: flex;
   align-items: center;
@@ -106,6 +108,10 @@ const navVariants = {
   },
 };
 
+interface IForm {
+  keyword: string;
+}
+
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false);
   const inputAnimation = useAnimation();
@@ -134,6 +140,11 @@ function Header() {
       }
     });
   }, [scrollY, navAnimation]);
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<IForm>();
+  const onValid = (data: IForm) => {
+    navigate(`/search?keyword=${data.keyword}`);
+  };
   return (
     <Nav 
     animate={navAnimation} 
@@ -161,7 +172,7 @@ function Header() {
           </Items>
       </Col>
       <Col>
-      <Search>
+      <Search onSubmit={handleSubmit(onValid)}>
           <motion.svg
             onClick={toggleSearch}
             animate={{x: searchOpen ? -210 : 0}}
@@ -177,6 +188,7 @@ function Header() {
             ></path>
           </motion.svg>
           <Input 
+          {...register("keyword", {required:true, minLength: 2})}
           animate={inputAnimation}
           initial={{scaleX: 0}}
           transition={{type:"linear"}}
